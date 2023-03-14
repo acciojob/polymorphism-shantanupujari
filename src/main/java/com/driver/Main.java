b7667de34cdd9a38e615027b48ff3e498787cd03
shantanu <pujarishantanu33@gmail.com>
2023-03-14 14:56:50 +0530

    done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,26 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        Product p = new Product();
+        int ans = p.product(14,15);
+        int ans1= p.product(14,15,16);
+        double ans2= p.product(14.1,15.2);
+        System.out.println(ans+""+ans1+""+ans2);
 
+    }
+
+    public static class Product {
+        public int product(int x, int y) {
+            return x * y;
+        }
+
+        public int product(int x, int y, int z) {
+            return x * y * z;
+        }
+
+        public double product(double x, double y) {
+            return x * y;
+        }
+    }
 }
